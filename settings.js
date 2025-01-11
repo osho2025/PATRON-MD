9ada54358d35af9a1dd7c58c2066c9f441ba7d06
osho2025 <bellodivine135@gmail.com>
2025-01-11 20:49:23 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
-global.bankname = "MONIEPOINT"
+global.bankname = "UBA"
 
-global.banknumber = "8133729715"
+global.banknumber = "2340267307"
 
-global.bankowner = "FADARE"
+global.bankowner = "DIVINE BELLO ESHIKOGIE"
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
 global.autoRecord = false //auto recording (true to on, false to off)
